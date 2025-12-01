54ccd19d4d94c24c06bdc2342cffac4a9d96a52f
tdiamond01 <37713849+tdiamond01@users.noreply.github.com>
2025-12-01 15:54:57 -0700

    Add php-redis package, remove platform hooks

--- .platform/hooks/postdeploy/01_restart_queue.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Restart queue workers after deployment
-cd /var/app/current
-
-# Clear any cached jobs and restart workers
-php artisan queue:restart
-
-echo "Queue workers restarted successfully"
